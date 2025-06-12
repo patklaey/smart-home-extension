cc56e5130885ac4bba293ab46d42209a68a5cbe4
Kläy Pat, INI-CIT-SWS <Pat.Klaey@swisscom.com>
2025-06-12 08:49:17 +0200

    Fixes to astronomy client

--- internal/clients/astronomy.go
+++ internal/clients/astronomy.go
@@ -12,22 +12,22 @@ import (
 
 // See https://ipgeolocation.io/astronomy-api.html#fields-reference for more info on fields
 type Location struct {
-	Location_string       string  `json:"location_string,omitempty"`
-	Continent_code        string  `json:"continent_code,omitempty"`
-	Continent_name        string  `json:"continent_name,omitempty"`
-	Country_code2         string  `json:"country_code2,omitempty"`
-	Country_code3         string  `json:"country_code3,omitempty"`
-	Country_name          string  `json:"country_name,omitempty"`
-	Country_name_official string  `json:"country_name_official,omitempty"`
-	Is_eu                 bool    `json:"locais_eution_string,omitempty"`
-	State_prov            string  `json:"state_prov,omitempty"`
-	State_code            string  `json:"state_code,omitempty"`
-	District              string  `json:"district,omitempty"`
-	City                  string  `json:"city,omitempty"`
-	Locality              string  `json:"locality,omitempty"`
-	Zipcode               string  `json:"zipcode,omitempty"`
-	Latitude              float64 `json:"latitude"`
-	Longitude             float64 `json:"longitude"`
+	Location_string       string `json:"location_string,omitempty"`
+	Continent_code        string `json:"continent_code,omitempty"`
+	Continent_name        string `json:"continent_name,omitempty"`
+	Country_code2         string `json:"country_code2,omitempty"`
+	Country_code3         string `json:"country_code3,omitempty"`
+	Country_name          string `json:"country_name,omitempty"`
+	Country_name_official string `json:"country_name_official,omitempty"`
+	Is_eu                 bool   `json:"locais_eution_string,omitempty"`
+	State_prov            string `json:"state_prov,omitempty"`
+	State_code            string `json:"state_code,omitempty"`
+	District              string `json:"district,omitempty"`
+	City                  string `json:"city,omitempty"`
+	Locality              string `json:"locality,omitempty"`
+	Zipcode               string `json:"zipcode,omitempty"`
+	Latitude              string `json:"latitude"`  // wrong in docu
+	Longitude             string `json:"longitude"` // wrong in docu
 }
 
 type Astronomy struct {
@@ -79,11 +79,12 @@ func InitAstronomyClient(iBricksClient *IBricksClient, config *utils.Config) *As
 func (astronomyClient *AstronomyClient) StartUpdatingSunAzimuth(frequency int) {
 	go func() {
 		// Send initial heartbeat to let ibricks now we're here, then every frequency minute
-		for range time.Tick(time.Second * time.Duration(frequency)) {
+		for range time.Tick(time.Minute * time.Duration(frequency)) {
 			astronomyInfo, err := astronomyClient.getAstronomyInfo()
 			if err != nil {
 				logger.Error("Failed to get astronomy info, retrying in %d minutes", frequency)
 			} else {
+				logger.Trace("Successfully fetched astronomy info: %v", astronomyInfo)
 				astronomyClient.iBricksClient.SetMemo(MemoSunAzimuth, astronomyInfo.Astronomy.Sun_azimuth)
 			}
 		}
